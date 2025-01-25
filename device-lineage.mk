a8fc5036235d57bb348c98fc1d7c8bbacd568d6d
Trijal Saha <trijalsaha2012@gmail.com>
2025-01-25 00:04:58 -0500

    Include BCR and ViPER4AndroidFX if they exist

--- device-lineage.mk
+++ device-lineage.mk
@@ -19,6 +19,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# DeviceAsWebcam
+TARGET_BUILD_DEVICE_AS_WEBCAM := true
+
 # Elmyra
 PRODUCT_PACKAGES += \
     ElmyraService
@@ -75,3 +78,10 @@ PRODUCT_PACKAGES += \
 
 # Properties
 TARGET_VENDOR_PROP := $(LOCAL_PATH)/vendor.prop
+
+## Extras/Add-ons
+# ViPER4AndroidFX
+$(call inherit-product-if-exists, packages/apps/ViPER4AndroidFX/config.mk)
+
+# Basic Call Recorder (BCR)
+$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
